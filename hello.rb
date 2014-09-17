984e0c681b0eccdb0646219c17a31593fa0bad26
OBrienLT <liam.t.obrien@hotmail.com>
2014-09-16 23:10:09 -0400

    Added a comment

--- hello.rb
+++ hello.rb
@@ -1,10 +1,4 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1265\cocoasubrtf210
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural
+# Default is "World"
+name = ARGV.first || "World"
 
-\f0\fs24 \cf0 name = ARGV.first || \'93World\'94\
-\
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural
-\cf0 puts \'93Hello, #\{ARGV.first\}!\'94}
+puts "Hello, #{name}!"
